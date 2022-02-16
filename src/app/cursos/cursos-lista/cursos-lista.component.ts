da1ea17cda9991bbcb46677e2fe1ae56fa1eee83
Vinicius Rossi <vini.rossisilva@gmail.com>
2022-02-16 18:41:44 -0300

    feat: created method pipe async with method delay and configured called the API in environment

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,6 +1,7 @@
 import { CursosService } from './../cursos.service';
 import { Component, OnInit } from '@angular/core';
 import { Curso } from '../curso';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cursos-listas',
@@ -10,15 +11,20 @@ import { Curso } from '../curso';
 })
 export class CursosListaComponent implements OnInit {
 
-  cursos: Curso[] | undefined;
+  // cursos: Curso[] | undefined;
+
+  cursos$: Observable<Curso[]>;
 
   constructor(
     private service: CursosService
   ) { }
 
   ngOnInit(): void {
-    this.service.list()
-    .subscribe(dados => this.cursos = dados);
+    // this.service.list()
+    // .subscribe(dados => this.cursos = dados);
+
+    this.cursos$ = this.service.list();
+
   }
 
 }
